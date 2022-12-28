d0b991f9059e94601456f0895bf009e3ac09e6b6
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-12-28 11:27:45 -0300

    H2 database, JPA, test profile

--- src/main/java/br/com/jeangabriel/webServiceSpring/entities/User.java
+++ src/main/java/br/com/jeangabriel/webServiceSpring/entities/User.java
@@ -1,10 +1,17 @@
 package br.com.jeangabriel.webServiceSpring.entities;
 
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Primary;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
