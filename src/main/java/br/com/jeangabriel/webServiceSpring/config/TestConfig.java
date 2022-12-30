1d7204e64d7e0605af2c3010c791961821ebd741
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-12-30 08:20:32 -0300

    OrderStatus Enum

--- src/main/java/br/com/jeangabriel/webServiceSpring/config/TestConfig.java
+++ src/main/java/br/com/jeangabriel/webServiceSpring/config/TestConfig.java
@@ -3,6 +3,7 @@ package br.com.jeangabriel.webServiceSpring.config;
 
 import br.com.jeangabriel.webServiceSpring.entities.Order;
 import br.com.jeangabriel.webServiceSpring.entities.User;
+import br.com.jeangabriel.webServiceSpring.entities.enums.OrderStatus;
 import br.com.jeangabriel.webServiceSpring.repositories.OrderRepository;
 import br.com.jeangabriel.webServiceSpring.repositories.UserRepository;
 import org.aspectj.weaver.ast.Or;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria", "maria@gmail.com", "12345678", "54321");
         User u2 = new User(null, "Alex Telles", "alex@gmail.com", "5487678", "54aa1");
 
-        Order o1 = new Order(null, Instant.parse("2022-12-28T16:15:49Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2022-11-28T12:55:49Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2022-10-28T10:35:49Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2022-12-28T16:15:49Z"), u1, OrderStatus.WAITING_PAYMENT);
+        Order o2 = new Order(null, Instant.parse("2022-11-28T12:55:49Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2022-10-28T10:35:49Z"), u1, OrderStatus.SHIPPED);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
